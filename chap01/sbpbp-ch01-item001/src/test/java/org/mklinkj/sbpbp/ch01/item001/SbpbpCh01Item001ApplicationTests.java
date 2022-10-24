2fa653fb777667f0fd16f2993f6a4b45bc2133fc
mklinkj <96992046+mklinkj@users.noreply.github.com>
2022-10-24 23:43:56 +0900

    Gradle용 lombok 플러그인 사용하도록 변경
    https://github.com/mklinkj/spring-boot-persistence-best-practices-study/issues/1

--- chap01/sbpbp-ch01-item001/src/test/java/org/mklinkj/sbpbp/ch01/item001/SbpbpCh01Item001ApplicationTests.java
+++ chap01/sbpbp-ch01-item001/src/test/java/org/mklinkj/sbpbp/ch01/item001/SbpbpCh01Item001ApplicationTests.java
@@ -1,11 +1,14 @@
 package org.mklinkj.sbpbp.ch01.item001;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class SbpbpCh01Item001ApplicationTests {
-
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    LOGGER.info("안녕하세요! JPA");
+  }
 }
